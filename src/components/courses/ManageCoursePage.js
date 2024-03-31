437c1cd98d98b503d6548f895d1c955ed8348ead
Benjamin Saks <saksben2@gmail.com>
2024-03-30 20:33:45 -0700

    make tests for Redux

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -9,7 +9,7 @@ import Spinner from "../common/Spinner";
 import { toast } from "react-toastify";
 
 // function-based React component
-function ManageCoursePage({
+export function ManageCoursePage({
   courses,
   authors,
   loadAuthors,
